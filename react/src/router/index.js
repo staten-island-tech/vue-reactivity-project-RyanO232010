fcfc9fc209b1142da986ae6fe47864aab6a5d3c0
RyanO232010 <ryano92@nycstudents.net>
2026-02-11 12:28:08 -0500

    1

--- react/src/router/index.js
+++ react/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import UserCreate from '@/views/UserCreate.vue'
+import MainPage from '@/views/MainPage.vue'
 
 
 const router = createRouter({
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: UserCreate,
+      component: MainPage,
     },
   ],
 })
---
+++ react/src/views/MainPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>The Recipe Book</h1>
+    <div class="bcontainer"></div>
+  </div>
+</template>
+
+<script setup>
+const alp = "abcdefghijklmnopqrstuvwxyz".split("");
+
+function injectButtons() {
+  bcontainer.innerHTML = "";
+
+  alp.forEach(letter => {
+    bcontainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="letter-btn p-2 bg-red-600 m-1 px-3 py-1.5 rounded" data-letter="${letter}">
+        ${letter.toUpperCase()}
+      </button>`
+    );
+  });
+}
+
+injectButtons();
+</script>
+
+<style scoped>
+h1,
+h2 {
+  text-align: center;
+}
+</style>
